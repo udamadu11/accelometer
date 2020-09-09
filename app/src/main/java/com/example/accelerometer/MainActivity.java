9724cca27c70ae9d8e5873fcb4fb7bc0b079f454
udamadu11 <madu12dara@gmail.com>
2020-09-09 22:38:41 +0530

    velosity c

--- app/src/main/java/com/example/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/accelerometer/MainActivity.java
@@ -24,7 +24,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     ArrayList<Float> z = new ArrayList<Float>(10);
     ArrayList<Float> x = new ArrayList<Float>(10);
     ArrayList<Float> y = new ArrayList<Float>(10);
-    Float v;
+    ArrayList<Float> v = new ArrayList<Float>(10);
+    ArrayList<Float> velocity = new ArrayList<Float>(10);
+    float current = 0,prev = 0,interval = 0, velosity = 0;
+    Float nanosecond = 1.0f / 1000000.0f;
+    float mul = 0;
 
     private long timestamp;
 
@@ -41,8 +45,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
 
         z.add(event.values[0]);
-
-        x.add((float) (SystemClock.elapsedRealtime() /100000L ));
+        x.add((float)event.timestamp);
         if(z.size() == 10){
             sm.unregisterListener(this,accerometer);
         }
@@ -50,17 +53,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         list = findViewById(R.id.list);
         list2 = findViewById(id.list2);
         list3 = findViewById(id.list3);
-        float mul = 0;
-        for(int i=0; i<x.size(); i++) {
-           float num1 = z.get(i);
-           float num2 = x.get(i);
-           mul = num1 * num2;
-           
-        }
-        y.add(mul);
 
-        Log.d("Array List: ", String.valueOf(z));
-        Log.d("Array List: ", String.valueOf(x));
+        getV();
+        getInterval();
+        getVelocity();
+
+        Log.d("Array List z: ", String.valueOf(z));
+        Log.d("Array List x: ", String.valueOf(x));
+        Log.d("Array List v: ", String.valueOf(v));
+        Log.d("Array List velocity: ", String.valueOf(velocity));
 
         ArrayAdapter adapter = new ArrayAdapter<Float>(this,
                 android.R.layout.simple_list_item_1, z);
@@ -68,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
         ArrayAdapter adapter2 = new ArrayAdapter<Float>(this,
-                android.R.layout.simple_list_item_1, x);
+                android.R.layout.simple_list_item_1, v);
         list2.setAdapter(adapter2);
 
         ArrayAdapter adapter3 = new ArrayAdapter<Float>(this,
@@ -78,8 +79,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
+    public void getInterval(){
+        for(int i=0; i<x.size(); i++) {
+            current = x.get(i);
+            if(prev == 0) prev = current;
+            interval = (current - prev) * nanosecond;
+            prev = current;
+        }
+        v.add(interval);
+    }
+    public void getV(){
+        for(int i=0; i<x.size(); i++) {
+            float num1 = z.get(i);
+            float num2 = x.get(i);
+            mul = num1 * num2;
 
-
+        }
+        y.add(mul);
+    }
+    public void getVelocity(){
+        for(int i=0; i<x.size(); i++) {
+            float in = z.get(i); // get time intervals
+            float acc = v.get(i); // get acceration from arraylist
+            velosity = in * acc;
+        }
+        velocity.add(velosity);
+    }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
