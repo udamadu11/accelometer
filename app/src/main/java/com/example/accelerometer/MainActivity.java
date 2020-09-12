8eaf8e524b1838516ee36c20c40bcee3fcdd1fc6
udamadu11 <madu12dara@gmail.com>
2020-09-12 12:55:13 +0530

    add button

--- app/src/main/java/com/example/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/accelerometer/MainActivity.java
@@ -12,6 +12,8 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import static com.example.accelerometer.R.*;
@@ -21,17 +23,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Sensor accerometer;
     SensorManager sm;
     ListView list,list2,list3;
+
     ArrayList<Float> z = new ArrayList<Float>(10);
     ArrayList<Float> x = new ArrayList<Float>(10);
-    ArrayList<Float> y = new ArrayList<Float>(10);
     ArrayList<Float> v = new ArrayList<Float>(10);
+
     ArrayList<Float> velocity = new ArrayList<Float>(10);
+
     float current = 0,prev = 0,interval = 0, velosity = 0;
     Float nanosecond = 1.0f / 1000000.0f;
-    float mul = 0;
-
+    float mul = 0, inter;
+    private static DecimalFormat df = new DecimalFormat("0");
     private long timestamp;
 
+    // called activity is first created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,9 +49,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        z.add(event.values[0]);
-        x.add((float)event.timestamp);
-        if(z.size() == 10){
+        z.add(event.values[0]); // add to acceleration into z arrayList
+        x.add((float)event.timestamp); // add timestamp into x arrayList
+        if(z.size() == 10){ //adding acceleration value until 10
             sm.unregisterListener(this,accerometer);
         }
 
@@ -54,56 +59,51 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         list2 = findViewById(id.list2);
         list3 = findViewById(id.list3);
 
-        getV();
-        getInterval();
-        getVelocity();
+        getInterval(); // call getInterval() to count time interval
+        getVelocity(); // call getVelocity?() to count velocity
 
-        Log.d("Array List z: ", String.valueOf(z));
-        Log.d("Array List x: ", String.valueOf(x));
-        Log.d("Array List v: ", String.valueOf(v));
-        Log.d("Array List velocity: ", String.valueOf(velocity));
+//        Log.d("Array List z: ", String.valueOf(z));
+//        Log.d("Array List x: ", String.valueOf(x));
+//        Log.d("Array List v: ", String.valueOf(v));
+//        Log.d("Array List velocity: ", String.valueOf(velocity));
 
+        // print acceleration arrayList value into mobile display
         ArrayAdapter adapter = new ArrayAdapter<Float>(this,
                 android.R.layout.simple_list_item_1, z);
         list.setAdapter(adapter);
 
-
+        // print time interval arrayList value into mobile display
         ArrayAdapter adapter2 = new ArrayAdapter<Float>(this,
                 android.R.layout.simple_list_item_1, v);
         list2.setAdapter(adapter2);
 
+        // print velocity arrayList value into mobile display
         ArrayAdapter adapter3 = new ArrayAdapter<Float>(this,
-                android.R.layout.simple_list_item_1, y);
+                android.R.layout.simple_list_item_1, velocity);
         list3.setAdapter(adapter3);
 
     }
 
-
+// get time interval into arrayList
     public void getInterval(){
         for(int i=0; i<x.size(); i++) {
-            current = x.get(i);
-            if(prev == 0) prev = current;
-            interval = (current - prev) * nanosecond;
-            prev = current;
+            current = x.get(i); // get arrayList i value
+            if(prev == 0) prev = current; // check previous value is 0 or not
+            interval = (current - prev) * nanosecond; // count interval and convert to nanoSecond
+            df.setRoundingMode(RoundingMode.UP); //round value
+            prev = current; // current value assign to previous value
         }
-        v.add(interval);
+        v.add(Float.valueOf(df.format(interval))); // add interval to v arrayList
     }
-    public void getV(){
-        for(int i=0; i<x.size(); i++) {
-            float num1 = z.get(i);
-            float num2 = x.get(i);
-            mul = num1 * num2;
 
-        }
-        y.add(mul);
-    }
+    // count velocity
     public void getVelocity(){
         for(int i=0; i<x.size(); i++) {
-            float in = z.get(i); // get time intervals
-            float acc = v.get(i); // get acceration from arraylist
-            velosity = in * acc;
+            float acc = z.get(i); // get acceleration value from arrayList
+            float in = v.get(i); // get intervals  from v arrayList
+            velosity = in * acc; // count velocity (acceleration * time interval )
         }
-        velocity.add(velosity);
+        velocity.add(velosity); // add velocity to velocity arrayList
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
