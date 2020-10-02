99ac97e1c018e2b88241da6c4374e2b48869ad8d
udamadu11 <madu12dara@gmail.com>
2020-10-02 23:03:46 +0530

    part 2.2

--- app/src/main/java/com/example/accelerometer/MainActivity2.java
+++ app/src/main/java/com/example/accelerometer/MainActivity2.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,13 +14,16 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
     ArrayList<Float> mList1,mList2;
     ListView list,list2,list3;
     float currentValue = 0 , newValue = 0;
-    ArrayList<Float> direction = new ArrayList<Float>(10);
+    ArrayList<String> direction = new ArrayList<String>(10);
+    private static DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,7 @@ public class MainActivity2 extends AppCompatActivity {
         list2 = findViewById(R.id.list2);
         list3 = findViewById(R.id.list3);
 
+        //retrieve sharedPreferences data
         SharedPreferences sharedPreferences = getSharedPreferences("accelerometer", MODE_PRIVATE);
         Gson gson = new Gson();
         String json = sharedPreferences.getString("list1", null);
@@ -43,32 +46,43 @@ public class MainActivity2 extends AppCompatActivity {
         mList1 = gson.fromJson(json, type);
         mList2 = gson.fromJson(json2, type2);
 
+        // show acceleration data
         ArrayAdapter adapter = new ArrayAdapter<Float>(this,
                 android.R.layout.simple_list_item_activated_1, mList1);
         list.setAdapter(adapter);
 
+        //show time interval
         ArrayAdapter adapter2 = new ArrayAdapter<Float>(this,
                 android.R.layout.simple_list_item_activated_1, mList2);
         list2.setAdapter(adapter2);
 
+        //calculate direction indicator
         for(int i=0;i<10;i++){
-//            currentValue = mList1.get(i);
-            newValue = 5;
-            direction.add(newValue);
+           currentValue = mList1.get(i);
+            newValue = currentValue - 0;
+            df.setRoundingMode(RoundingMode.DOWN);
+            float value = Float.valueOf(df.format(newValue));
+            if(value > 0 ){
+                direction.add("+" + value);
+            }else{
+                direction.add(""+value);
+            }
+
         }
 
-
+    //save direction data in sharePreferences
         SharedPreferences sp = getSharedPreferences("accelerometer",MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         Gson gsonNew = new Gson();
         String json1 = gsonNew.toJson(direction);
         editor.putString("list3",json1);
         editor.commit();
-
-        ArrayAdapter adapter3 = new ArrayAdapter<Float>(this,
+    //show direction indicator data as listView
+        ArrayAdapter adapter3 = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_activated_1, direction);
         list3.setAdapter(adapter3);
 
+        //click button to erase data
         Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener(){
 
@@ -79,8 +93,11 @@ public class MainActivity2 extends AppCompatActivity {
         });
     }
     public void eraseData(){
+        //remove data for SharedPreferences
         SharedPreferences settings = getSharedPreferences("accelerometer", MODE_PRIVATE);
         settings.edit().clear().commit();
+
+        //Refresh the screen
         Intent ii = new Intent(MainActivity2.this, MainActivity2.class);
         finish();
         overridePendingTransition(0, 0);
